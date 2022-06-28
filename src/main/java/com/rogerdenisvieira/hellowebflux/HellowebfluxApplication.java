4e229127a7ecece8d73923a8747819004492231e
Roger Vieira <rogerdenis@gmail.com>
2022-06-28 00:14:01 -0300

    project scaffold

---
+++ src/main/java/com/rogerdenisvieira/hellowebflux/HellowebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.rogerdenisvieira.hellowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowebfluxApplication.class, args);
+	}
+
+}
